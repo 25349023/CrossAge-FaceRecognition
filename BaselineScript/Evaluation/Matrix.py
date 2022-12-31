92830a9df5f65cc42f518569a96a7215cbc6f6ec
Ben.Chen <goodben12345@gmail.com>
2022-12-31 17:41:00 +0800

    Fix R1 Acc

--- BaselineScript/Evaluation/Matrix.py
+++ BaselineScript/Evaluation/Matrix.py
@@ -37,30 +37,43 @@ def get_auc(pred_similarity, ground_truth):
     return metrics.roc_auc_score(ground_truth, pred_similarity)
 
 
-def get_rank_one(pred_similarity, ground_truth, len_features):
+def get_rank_one(pred_similarity, ground_truth, num_faces):
     ''' this function return the rank-1 accuracy of the input
     
     Args:
         pred_similarity (numpy_array, size:(B, 1)): 
             the predicted similarity results
-            B should be C(len_features, 2)
+            B should be C(num_faces, 2)
 
         ground_truth (numpy_array, size:(B, 1)): 
             the ground truth of whether each pair is same or not (same=1, not_same=0)
-            B should be C(len_features, 2)
+            B should be C(num_faces, 2)
 
-        len_features (int):
-            the number of length of the features(faces)
+        num_faces (int):
+            the number of the faces
     '''
 
+    # Rebuild nxn similarity and ground_truth matrix
+    sim_map = np.zeros((num_faces, num_faces))
+    gt_map = np.zeros((num_faces, num_faces))
     used_pairs = 0
-    total_success = 0
-    for cur_num_pairs in range(len_features - 1, 1, -1):
-        # find out current face pairs with others
+    for i in range(num_faces - 1):
+        cur_num_pairs = num_faces - i - 1
         target = pred_similarity[used_pairs:used_pairs + cur_num_pairs]
-        # check if the most similar pair is actually the same person 
-        success = ground_truth[np.argmax(target) + used_pairs]
+        target_truth = ground_truth[used_pairs:used_pairs + cur_num_pairs]
 
+        sim_map[i, i + 1:] = target
+        sim_map[i + 1:, i] = target
+        gt_map[i, i + 1:] = target_truth
+        gt_map[i + 1:, i] = target_truth
+
+        used_pairs += cur_num_pairs
+
+    total_success = 0
+    for i in range(num_faces):
+        j = np.argmax(sim_map[i]) # most similar to face i
+
+        success = gt_map[i][j]
         total_success += success
 
-    return float(total_success) / len_features
+    return float(total_success) / num_faces
