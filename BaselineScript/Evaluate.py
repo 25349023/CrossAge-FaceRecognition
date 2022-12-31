14e1a5624c8e78ac75c8499ec8a96d194c309f59
sky9023 <25349023.qq@gmail.com>
2022-12-31 17:11:38 +0800

    [MISC] reformat code; updated gitignore; use gpu to evaluate

--- BaselineScript/Evaluate.py
+++ BaselineScript/Evaluate.py
@@ -4,18 +4,19 @@ import tqdm
 import torch
 
 
-
 def evaluate(model, dataset, dataloader=None):
-    model = model.cpu()
+    # model = model
     model.eval()
 
     if dataloader == None:
         dataloader = DataLoader(dataset, 8, num_workers=2)
-    
-    features = []
-    for i, (pics, labels) in enumerate(tqdm.tqdm(dataloader)):
-        features.append(model(pics).detach())
-    features = torch.cat(features, dim=0).detach()
+
+    with torch.no_grad():
+        features = []
+        for i, (pics, labels) in enumerate(tqdm.tqdm(dataloader)):
+            pics = pics.to('cuda')
+            features.append(model(pics))
+        features = torch.cat(features, dim=0).cpu()
 
     '''
     gt_pair: ground truth of if pair i are the same person. 0 is false and 1 is true.
@@ -26,9 +27,9 @@ def evaluate(model, dataset, dataloader=None):
     ids = dataset.identities
 
     for i in range(len(ids)):
-        for j in range(i+1, len(ids)):
+        for j in range(i + 1, len(ids)):
             gt_pair.append(1 * (ids[i] == ids[j]))
-        pd_pair.append(Matrix.get_cosine_similarity(features[i:i+1], features[i+1:])[0])
+        pd_pair.append(Matrix.get_cosine_similarity(features[i:i + 1], features[i + 1:])[0])
 
     pd_pair = np.concatenate(pd_pair, axis=0)
 
@@ -37,6 +38,7 @@ def evaluate(model, dataset, dataloader=None):
 
     return auc, r1_acc
 
+
 if __name__ == "__main__":
     from dataloader.dataset import CALFWDataset
     from torch.utils.data import DataLoader
@@ -44,10 +46,10 @@ if __name__ == "__main__":
     from ModelFactory import FaceFeatureExtractor
 
     facerecognition = FaceFeatureExtractor.insightFace("mobilefacenet")
-    model = facerecognition.model
+    model = facerecognition.model.to('cuda')
 
-    dataset = CALFWDataset('..\\\data\\calfw', 'ForTraining\\CALFW_validationlist.csv',
-                        transform=T.Compose([T.CenterCrop(112), T.ToTensor()]))
+    dataset = CALFWDataset('..\\data\\calfw', 'ForTraining\\CALFW_validationlist.csv',
+                           transform=T.Compose([T.CenterCrop(112), T.ToTensor()]))
 
     auc, r1_acc = evaluate(model, dataset)
     print(f"use pretained model mobilefacenet")
--- BaselineScript/Evaluation/Matrix.py
+++ BaselineScript/Evaluation/Matrix.py
@@ -5,7 +5,7 @@ def get_cosine_similarity(feat1, feat2):
     feat1 = feat1 / np.linalg.norm(feat1)
     feat2 = feat2 / np.linalg.norm(feat2)
     result = np.dot(feat1, feat2.T)
-    
+
     return result
 
 
@@ -36,6 +36,7 @@ def get_auc(pred_similarity, ground_truth):
 
     return metrics.roc_auc_score(ground_truth, pred_similarity)
 
+
 def get_rank_one(pred_similarity, ground_truth, len_features):
     ''' this function return the rank-1 accuracy of the input
     
@@ -54,12 +55,12 @@ def get_rank_one(pred_similarity, ground_truth, len_features):
 
     used_pairs = 0
     total_success = 0
-    for cur_num_pairs in range(len_features-1, 1, -1):
+    for cur_num_pairs in range(len_features - 1, 1, -1):
         # find out current face pairs with others
-        target = pred_similarity[used_pairs:used_pairs+cur_num_pairs]
+        target = pred_similarity[used_pairs:used_pairs + cur_num_pairs]
         # check if the most similar pair is actually the same person 
-        success = ground_truth[np.argmax(target)+used_pairs]
+        success = ground_truth[np.argmax(target) + used_pairs]
 
         total_success += success
-    
+
     return float(total_success) / len_features
